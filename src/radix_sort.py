f49b62ff625f751c8429d0f86e3b1b60107600fe
azerata <smedemarkhammer@gmail.com>
2022-11-04 14:52:44 +0100

    Cleaned up the excessive slicing in radix

--- src/radix_sort.py
+++ src/radix_sort.py
@@ -1,11 +1,13 @@
+from dataclasses import dataclass
 
-def radix_sort(inp: "list[str]", alphabet="$abcdefghijklmnopqrstuvwxyz") -> "list[str]":
+
+def radix_sort(inp: "list[suffix]", alphabet="$abcdefghijklmnopqrstuvwxyz") -> "list[suffix]":
     '''
     this implementation assumes all input strings are of length m.
     also needs an (ordered) alphabet to run, english alphabet + sentinel added as default...
     '''
     m = [i-1 for i in range(len(inp[0]), 0, -1)]
-    arr = [None for _ in inp]
+    arr: list[None | suffix] = [None for _ in inp]
     buckets = {i: 0 for i in alphabet}
 
     for j in m:
@@ -21,6 +23,27 @@ def radix_sort(inp: "list[str]", alphabet="$abcdefghijklmnopqrstuvwxyz") -> "lis
             buckets[x[j]] += 1
         arr, inp = inp, arr
     return inp
+
+
+@dataclass
+class suffix:
+    x: str
+    i: int
+
+    def __getitem__(self, j: int) -> str:
+        if not self.x or self.i + j >= len(self.x):
+            return '$'
+        return self.x[(self.i+j) % len(self.x)]
+
+    def __len__(self) -> int:
+        return len(self.x)
+
+    def __str__(self) -> str:
+        return self.x[self.i:]
+
+
+def radix_bedre(inp: list[int], x: str) -> list[int]:
+    pass
 
 
 def alphabet_from_input_string(x: str) -> str:
